3e9e9e44e4fd3ba6e61fad05bdf2ed13c49e4d1d
Aster <galaster@foxmail.com>
2022-12-14 11:39:44 +0800

    Support password authenticate

--- projects/swarm-overlord/src/overload/mod.rs
+++ projects/swarm-overlord/src/overload/mod.rs
@@ -1,19 +1,52 @@
-use std::fs;
-use std::io::{Read, Write};
-use std::net::TcpStream;
-use std::path::Path;
+use std::{
+    fs,
+    io::{Read, Write},
+    net::{TcpStream, ToSocketAddrs},
+    path::Path,
+};
 
+use diagnostic_quick::QResult;
 use ssh2::Session;
 
-const FILE_PATH: &str = "Cargo.toml";
+pub struct OverloadClient {
+    session: Session,
+}
 
-fn main() {
-    let tcp = TcpStream::connect("192.168.1.128:22").unwrap();
+impl OverloadClient {
+    pub fn login_password<A>(address: A, user: &str, password: &str) -> QResult<Self>
+    where
+        A: ToSocketAddrs,
+    {
+        let tcp = TcpStream::connect(address)?;
+        let mut session = Session::new()?;
+        session.set_tcp_stream(tcp);
+        session.handshake()?;
+        session.userauth_password(user, password)?;
+        if session.authenticated() {
+            Ok(Self { session })
+        }
+        else {
+            return Err("Authentication failed".into());
+        }
+    }
+}
+
+impl Drop for OverloadClient {
+    fn drop(&mut self) {
+        todo!()
+    }
+}
+
+#[tokio::test]
+async fn main() -> QResult {
+    OverloadClient::login_password("192.168.1.100:22", "root", "projecta")?;
+
+    let tcp = TcpStream::connect("192.168.1.100:22")?;
     let mut session = Session::new().unwrap();
     session.set_tcp_stream(tcp);
     session.handshake().unwrap();
 
-    session.userauth_password("root", "123456").unwrap();
+    session.userauth_password("root", "projecta").unwrap();
     session.authenticated();
 
     let mut channel = session.channel_session().unwrap();
@@ -26,21 +59,19 @@ fn main() {
     channel.wait_close().unwrap();
 
     // 上传文件
-    let result = fs::read(FILE_PATH).unwrap();
-    let mut remote_file = session.scp_send(Path::new(FILE_PATH)
-                                           , 0o644, result.len() as u64, None).unwrap();
+    let result = fs::read("Cargo.toml").unwrap();
+    let mut remote_file = session.scp_send(Path::new("Cargo.toml"), 0o755, result.len() as u64, None).unwrap();
     remote_file.write(&result).unwrap();
 
     // 下载文件
-    let (mut remote_file, _) = session.scp_recv(Path::new(FILE_PATH))
-        .unwrap();
+    let (mut remote_file, _) = session.scp_recv(Path::new("Cargo.toml")).unwrap();
     let mut read = Vec::new();
     remote_file.read_to_end(&mut read).unwrap();
-    println!("{:?}", String::from_utf8(read).unwrap());
 
     // 关闭频道，等待全部内容传输完毕
     remote_file.send_eof().unwrap();
     remote_file.wait_eof().unwrap();
     remote_file.close().unwrap();
     remote_file.wait_close().unwrap();
+    Ok(())
 }
